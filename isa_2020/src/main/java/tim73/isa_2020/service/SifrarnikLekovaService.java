f848ffd4560cb79e42050c890c3ceaa2edb3a31b
Olga Momirov <olgamomirov@gmail.com>
2020-12-30 20:18:58 +0100

    prilagodjene alergije novom modelu

--- isa_2020/src/main/java/tim73/isa_2020/service/SifrarnikLekovaService.java
+++ isa_2020/src/main/java/tim73/isa_2020/service/SifrarnikLekovaService.java
@@ -1,9 +1,15 @@
 package tim73.isa_2020.service;
 
+import java.util.List;
+
 import tim73.isa_2020.model.SifrarnikLekova;
 
 public interface SifrarnikLekovaService {
 
 	
 	SifrarnikLekova getById(Long id);
+	
+	List<SifrarnikLekova> findAll();
+	
+	SifrarnikLekova findByNaziv(String naziv);
 }
