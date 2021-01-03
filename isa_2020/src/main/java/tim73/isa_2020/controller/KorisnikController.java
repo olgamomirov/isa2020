28de47e2f6f79d8034ab5f2ceab05bb13fbd4cbd
Olga Momirov <olgamomirov@gmail.com>
2021-01-03 21:01:21 +0100

    rezervacija pregleda kod dermatologa i dopuna profila apoteke

--- isa_2020/src/main/java/tim73/isa_2020/controller/KorisnikController.java
+++ isa_2020/src/main/java/tim73/isa_2020/controller/KorisnikController.java
@@ -33,6 +33,7 @@ import org.springframework.web.bind.annotation.RestController;
 import tim73.isa_2020.dto.ApotekaDTO;
 import tim73.isa_2020.dto.DermatologDTO;
 import tim73.isa_2020.dto.FarmaceutDTO;
+import tim73.isa_2020.dto.LekarDTO;
 import tim73.isa_2020.dto.PacijentPodaciDTO;
 import tim73.isa_2020.dto.PregledDTO;
 import tim73.isa_2020.model.Apoteka;
@@ -45,6 +46,7 @@ import tim73.isa_2020.model.Pregled;
 import tim73.isa_2020.model.UserTokenState;
 import tim73.isa_2020.repository.PacijentRepository;
 import tim73.isa_2020.securityService.TokenUtils;
+import tim73.isa_2020.service.ApotekaService;
 import tim73.isa_2020.service.KorisnikService;
 import tim73.isa_2020.service.KorisnikServiceImpl;
 import tim73.isa_2020.service.PacijentService;
@@ -231,4 +233,5 @@ public class KorisnikController {
   		return new ResponseEntity<List<PacijentPodaciDTO>>(pacijentiDTO, HttpStatus.OK);
   	}
 
+	
 }
