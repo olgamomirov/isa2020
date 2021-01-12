e69e6e59978a51b421634974c21c6d7b28044780
VioletaMarceta <violetamarceta1995@gmail.com>
2021-01-12 21:02:44 +0100

    radno vreme

--- isa_2020/src/main/java/tim73/isa_2020/dto/RadnoVremeDTO.java
+++ isa_2020/src/main/java/tim73/isa_2020/dto/RadnoVremeDTO.java
@@ -8,6 +8,8 @@ import tim73.isa_2020.model.RadnoVreme;
 
 public class RadnoVremeDTO {
 	
+
+	private Long id;
 	
 	private DateTime start = new DateTime();
 	
@@ -16,11 +18,12 @@ public class RadnoVremeDTO {
 	private Interval interval  = new org.joda.time.Interval( start, stop );
 	
 	public RadnoVremeDTO (RadnoVreme rv) {
-		this(rv.getInterval());
+		this(rv.getInterval(), rv.getId());
 	}
 
-	public RadnoVremeDTO(Interval interval2) {
+	public RadnoVremeDTO(Interval interval2, Long id) {
 		this.interval = interval2;
+		this.id = id;
 	}
 
 	public Interval getInterval() {
@@ -30,7 +33,13 @@ public class RadnoVremeDTO {
 	public void setInterval(Interval interval) {
 		this.interval = interval;
 	}
-	
-	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 }
