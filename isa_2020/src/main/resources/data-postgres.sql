7b470ac0efb065af4b60ed4002da05a93a60ea1c
Olga Momirov <olgamomirov@gmail.com>
2021-01-12 20:01:31 +0100

    dodat pacijent u bazu

--- isa_2020/src/main/resources/data-postgres.sql
+++ isa_2020/src/main/resources/data-postgres.sql
@@ -24,6 +24,7 @@ insert into "public"."korisnik" ("id", "drzava", "email", "grad", "ime", "lozink
 
 insert into "public"."korisnik" ("id", "drzava", "email", "grad", "ime", "lozinka", "prezime", "telefon", "ulica", "enabled", "status") values(5, 'Srbija', 'aca@gmail.com', 'neki', 'Aca', '$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra', 'Stevic', '772555', 'Rumska', true, 'registrovan')
 
+insert into "public"."pacijent" ("id", "penal") values(3,0)
 
 
 insert into "public"."pacijent" ("id", "penal") values(5,0)
@@ -41,7 +42,7 @@ insert into "public"."korisnik_authority"  ("korisnik_id", "authority_id") value
 
 insert into "public"."tip_pregleda" ( "cena", "tip") values( 1000.0, 'pregled mladeza')
 insert into "public"."tip_pregleda" ( "cena", "tip") values( 500.0, 'kontrola')
-insert into "public"."pregled" ( "dijagnoza", "interval", "status", "terapija", "apoteka_id", "dermatolog_id", "farmaceut_id", "pacijent_id") values( 'zdravo', '2021-01-07T08:00:00.000+01:00/2021-01-07T15:00:00.000+01:00', 'odradjen', 'nema', 2, 2, null, 3)
+insert into "public"."pregled" ( "dijagnoza", "interval", "status", "terapija", "apoteka_id", "dermatolog_id", "farmaceut_id", "pacijent_id") values( 'zdravo', '', 'odradjen', 'nema', 2, 2, null, 3)
 insert into "public"."pregled" ( "dijagnoza", "interval", "status", "terapija", "apoteka_id", "dermatolog_id", "farmaceut_id", "pacijent_id") values( 'kontrola', '', 'odradjen', 'nema', 2, 2, null, 3)
 insert into "public"."pregled" ( "dijagnoza", "interval", "status", "terapija", "apoteka_id", "dermatolog_id", "farmaceut_id", "pacijent_id") values( 'strasno', '', 'odradjen', 'cccc', 2, null, 4, 3)
 insert into "public"."tipovi_pregledi" ("pregled_id", "tip_pregleda_id") values(1, 1)
