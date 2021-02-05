f5e516b1d6d5be13b86e34e929e055b0b7d4dab3
Olga Momirov <olgamomirov@gmail.com>
2021-02-05 16:40:35 +0100

    otpustanje farmaceuta

--- isa_2020/src/main/java/tim73/isa_2020/controller/FarmaceutiController.java
+++ isa_2020/src/main/java/tim73/isa_2020/controller/FarmaceutiController.java
@@ -6,19 +6,23 @@ import java.util.List;
 import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.websocket.server.PathParam;
 
 import org.joda.time.DateTime;
 import org.joda.time.Interval;
 import org.joda.time.Period;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -51,6 +55,8 @@ import tim73.isa_2020.service.PregledService;
 
 @RestController
 @RequestMapping(value = "/farmaceuti")
+@CrossOrigin(origins = "http://localhost:3000")
+
 public class FarmaceutiController {
 
 	@Autowired
@@ -349,4 +355,28 @@ public class FarmaceutiController {
 			
 			korisnikService.save(farmaceut);
 		}
-}
+		
+		@PutMapping(value = "/otpusti/{id}")
+		@PreAuthorize("hasRole('ADMINISTRATOR')")
+		public ResponseEntity<String> otpusti(@PathVariable Long id, HttpServletRequest request) {
+			System.out.println(id);
+			
+		
+		  Farmaceut f=farmaceutService.findOne(id); 
+		  if(!f.getPregledi().isEmpty()) {
+			  for (Pregled pregled: f.getPregledi()) {
+				  if(pregled.getStatus().equals("rezervisan")) {
+					  return ResponseEntity.badRequest().body("Farmaceut ima zakazane preglede");
+				  }
+			  }
+		  }
+		 f.setApoteka(null);
+		 korisnikService.save(f);
+		 return new ResponseEntity<String>("Farmaceut je otpusten!", HttpStatus.OK);
+			
+		}
+		
+		
+}		
+
+
